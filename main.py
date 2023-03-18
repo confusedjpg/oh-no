68b8477b89f2d84981abf98a46ad7916f67ed9fc
confusedjpg <dansajin06@gmail.com>
2023-03-19 00:02:43 +0100

    basic bot example

--- main.py
+++ main.py
@@ -5,3 +5,16 @@ from dotenv import load_dotenv
 load_dotenv()
 
 TOKEN = os.getenv("TOKEN")
+
+class Client(discord.Client):
+    async def on_ready(self):
+        print(f"Logged in as {self.user}")
+    
+    async def on_message(self, message):
+        print(f"Message from {message.author}: {message.content}")
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = Client(intents=intents)
+client.run(TOKEN)

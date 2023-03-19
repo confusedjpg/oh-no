4978fc3bd9f26556e713dd6a2c136c4f408cae36
confusedjpg <dansajin06@gmail.com>
2023-03-19 11:08:40 +0100

    switch to discord.ext
    this makes command creation and management much simpler and easier to develop

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import logging
 
 import discord
+from discord.ext import commands # easier command integration
 from dotenv import load_dotenv
 
 # set up env variables
@@ -12,12 +13,22 @@ TOKEN = os.getenv("TOKEN")
 # no need to keep logs of previous sessions, set mode to 'a' otherwise
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 
-class Client(discord.Client):
-    async def on_ready(self):
-        print(f"Logged in as {self.user}. Ready to operate!")
-
+# set required intents
+# for more privileged intents, going to the bot's dashboard might be required
+# for more info: https://discordpy.readthedocs.io/en/stable/intents.html
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = Client(intents=intents)
-client.run(TOKEN, log_handler=handler, log_level=logging.DEBUG)
+bot = commands.Bot(command_prefix=".", description="A general purpose bot.", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user} (ID: {bot.user.id}). \nReady to operate!")
+
+# add a listener to process messages asynchronously
+# don't handle commands here
+@bot.listen('on_message')
+async def printIt(message):
+    print(f"{message.author} said: {message.content}")
+
+bot.run(TOKEN, log_handler=handler, log_level=logging.DEBUG)

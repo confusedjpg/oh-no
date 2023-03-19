2859193d42e71e6e61774420b795836b2143dc1c
confusedjpg <dansajin06@gmail.com>
2023-03-19 10:48:11 +0100

    simple logging > file redirection

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 import os
+import logging
+
 import discord
 from dotenv import load_dotenv
 
+# set up env variables
 load_dotenv()
-
 TOKEN = os.getenv("TOKEN")
+
+# log everything to a file
+# no need to keep logs of previous sessions, set mode to 'a' otherwise
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 
 class Client(discord.Client):
     async def on_ready(self):
-        print(f"Logged in as {self.user}")
-    
-    async def on_message(self, message):
-        print(f"Message from {message.author}: {message.content}")
+        print(f"Logged in as {self.user}. Ready to operate!")
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = Client(intents=intents)
-client.run(TOKEN)
+client.run(TOKEN, log_handler=handler, log_level=logging.DEBUG)
